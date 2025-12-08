7a90a6655262e29a379629c1d7e1cae9fe832394
JarroVGIT <jarro@vanginkel-it.nl>
2025-12-08 20:49:04 +0100

    D8

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,4 +1,7 @@
-use std::{cmp::Ordering, collections::HashMap};
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet, VecDeque},
+};
 
 use itertools::Itertools;
 
@@ -21,34 +24,136 @@ fn distance(a: (usize, usize, usize), b: (usize, usize, usize)) -> f64 {
     (dx * dx + dy * dy + dz * dz).sqrt()
 }
 
-#[allow(unused_variables)]
-pub fn part_one(input: &str) -> Option<usize> {
-    let fuses: Vec<(usize, usize, usize)> = input.lines().map(|l| {
-        let mut line = l.split('-');
-        let x = line.next().unwrap().parse().unwrap();
-        let y = line.next().unwrap().parse().unwrap();
-        let z = line.next().unwrap().parse().unwrap();
-        (x,y,z)
-    }).collect_vec();
+pub fn solve_1(input: &str, connections: usize) -> Option<usize> {
+    let fuses: Vec<(usize, usize, usize)> = input
+        .lines()
+        .map(|l| {
+            let mut line = l.split(',');
+            let x = line.next().unwrap().parse().unwrap();
+            let y = line.next().unwrap().parse().unwrap();
+            let z = line.next().unwrap().parse().unwrap();
+            (x, y, z)
+        })
+        .collect_vec();
 
     let mut distances = HashMap::new();
     for a in 0..fuses.len() {
-        for b in (a+1)..fuses.len() {
+        for b in (a + 1)..fuses.len() {
             let dis = distance(fuses[a], fuses[b]);
-            distances.insert((a,b), Distance(dis));
+            distances.insert((a, b), Distance(dis));
         }
     }
 
-    let shortest: Vec<((usize, usize), Distance)> = distances.into_iter().sorted_by_key(|(_,v)| *v).take(1000).collect_vec();
+    let shortest = distances
+        .into_iter()
+        .sorted_by_key(|(_, v)| *v)
+        .take(connections);
 
-    
+    let mut adj: HashMap<usize, Vec<usize>> = HashMap::new();
+    for ((a, b), _) in shortest {
+        adj.entry(a).or_default().push(b);
+        adj.entry(b).or_default().push(a);
+    }
 
-    None
+    let mut seen = HashSet::new();
+    let mut components = vec![];
+
+    for idx in 0..fuses.len() {
+        if seen.contains(&idx) || !adj.contains_key(&idx) {
+            continue;
+        }
+
+        let mut new_component = HashSet::new();
+        let mut q = VecDeque::new();
+
+        q.push_back(idx);
+        seen.insert(idx);
+        while let Some(coord) = q.pop_front() {
+            new_component.insert(coord);
+            for neighbor in adj.get(&coord).unwrap() {
+                if seen.insert(*neighbor) {
+                    q.push_back(*neighbor);
+                }
+            }
+        }
+        components.push(new_component);
+    }
+
+    components.sort_by_key(|c| c.len());
+    let result: usize = components.iter().rev().take(3).map(|c| c.len()).product();
+
+    Some(result)
+}
+
+#[allow(unused_variables)]
+pub fn part_one(input: &str) -> Option<usize> {
+    solve_1(input, 1000)
 }
 
 #[allow(unused_variables)]
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let fuses: Vec<(usize, usize, usize)> = input
+        .lines()
+        .map(|l| {
+            let mut line = l.split(',');
+            let x = line.next().unwrap().parse().unwrap();
+            let y = line.next().unwrap().parse().unwrap();
+            let z = line.next().unwrap().parse().unwrap();
+            (x, y, z)
+        })
+        .collect_vec();
+
+    let total_boxes = input.lines().count();
+
+    let mut distances = HashMap::new();
+    for a in 0..fuses.len() {
+        for b in (a + 1)..fuses.len() {
+            let dis = distance(fuses[a], fuses[b]);
+            distances.insert((a, b), Distance(dis));
+        }
+    }
+    let mut shortest = distances
+        .into_iter()
+        .sorted_by_key(|(_, v)| *v)
+        .rev()
+        .map(|(k, _)| k)
+        .collect_vec();
+    let mut components: Vec<HashSet<usize>> = vec![];
+
+    loop {
+        let (a, b) = shortest.pop().unwrap();
+        let component_a_idx = components.iter().position(|c| c.contains(&a));
+        let component_b_idx = components.iter().position(|c| c.contains(&b));
+        match (component_a_idx, component_b_idx) {
+            (Some(c_a), Some(c_b)) => {
+                if c_a == c_b {
+                    continue;
+                } else {
+                    let new: HashSet<usize> =
+                        components[c_a].union(&components[c_b]).copied().collect();
+                    if c_a > c_b {
+                        components.remove(c_a);
+                        components.remove(c_b);
+                    } else {
+                        components.remove(c_b);
+                        components.remove(c_a);
+                    }
+
+                    components.push(new);
+                }
+            } //both already in component.
+            (Some(c_a), None) => {
+                components[c_a].insert(b);
+            }
+            (None, Some(c_b)) => {
+                components[c_b].insert(a);
+            }
+            (None, None) => components.push(HashSet::from([a, b])), // create new component.
+        }
+        if components.len() == 1 && components[0].len() == total_boxes {
+            return Some(fuses[a].0 * fuses[b].0);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -57,13 +162,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = solve_1(&advent_of_code::template::read_file("examples", DAY), 10);
+        assert_eq!(result, Some(40));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(25272));
     }
 }
